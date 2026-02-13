a1eadeffe54403b5cf794d478a04057f5b53e0d8
HailexTheHuman <cmyers@student.neumont.edu>
2026-02-13 16:08:23 -0700

    better starting structure

---
+++ Frontend/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/login', (req, res) => {
+    let model = {
+        username: '',
+        password: '',
+        message: ''
+    };
+
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    
+    if (username === 'admin' && password === 'password') {
+        res.send('Login successful!');
+    } else {
+
+        let model = {
+            username: username,
+            password: password,
+            message: 'Invalid credentials'
+        };
+        res.render('login', model);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
