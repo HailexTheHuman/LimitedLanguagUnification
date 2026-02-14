53c8c3c489938669a4d5369559ae4155a31b8643
cterry311 <cterry@student.neumont.edu>
2026-02-14 12:05:43 -0700

    Merged MainPage into DAL

---
+++ Backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
