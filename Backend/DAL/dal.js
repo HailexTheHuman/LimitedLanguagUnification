2e784688d20fc1ea013019244a273cca8377c8a0
Elaina <cmyers@student.neumont.edu>
2026-02-21 13:34:22 -0700

    removal of dummy method

--- Backend/DAL/dal.js
+++ Backend/DAL/dal.js
@@ -31,72 +31,6 @@ async function testConnection() {
     }
 }
 
-async function insertDummyUser() {
-    try {
-        await client.connect();
-        const dummyUser = {
-            username: "user",
-            password: "password",
-            email: "email@email.com",
-            conversations: [
-                {
-                    name: "test conversation",
-                    messages: [
-                        {
-                            sender: "user",
-                            text: "Hello World!"
-                        },
-                        {
-                            sender: "model",
-                            text: "Hello Back!"
-                        }
-                    ]
-                },
-                {
-                    name: "another conversation",
-                    messages: [
-                        {
-                            sender: "user",
-                            text: "how many plumbers does it take to change a light bulb?"
-                        },
-                        {
-                            sender: "model",
-                            text: "I have no idea."
-                        }
-                    ]
-                }
-            ]
-        }
-        await client.db("LLU").collection("users").insertOne(dummyUser);
-    } finally {
-        await client.close();
-    }
-}
-
-async function getUsers() {
-    try {
-        await client.connect();
-        console.log(await client.db("LLU").collection("users").find().toArray());
-    } finally {
-        await client.close();
-    }
-}
-
-async function getUserByUsername(username) {
-    try {
-        await client.connect();
-        return await client.db("LLU").collection("users").findOne({username: username});
-    } finally {
-        await client.close();
-    }
-}
-
-async function getResponse(context, prompt, model) {
-    return {response: "I am responding!"}
-}
-
-
-
 async function createUser(username, password) {
     try {
         await client.connect();
@@ -140,12 +74,37 @@ async function createUser(username, password) {
     }
 }
 
+async function getUsers() {
+    try {
+        await client.connect();
+        console.log(await client.db("LLU").collection("users").find().toArray());
+    } finally {
+        await client.close();
+    }
+}
+
+async function getUserByUsername(username) {
+    try {
+        await client.connect();
+        return await client.db("LLU").collection("users").findOne({username: username});
+    } finally {
+        await client.close();
+    }
+}
+
+async function getResponse(context, prompt, model) {
+    return {response: "I am responding!"}
+}
+
+
+
+
+
 
 
 
 
 exports.testConnection = testConnection;
-exports.insertDummyUser = insertDummyUser;
 exports.createUser = createUser;
 exports.getUsers = getUsers;
 exports.getUserByUsername = getUserByUsername;
